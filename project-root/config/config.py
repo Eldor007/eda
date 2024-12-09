dddd3b6c027407a1e3f9b21b559fc302083f369b
Eldor <126213105+Eldor007@users.noreply.github.com>
2024-12-10 02:52:32 +0500

    Update config.py

--- project-root/config/config.py
+++ project-root/config/config.py
@@ -1,1 +1,1 @@
-DATABASE_URL = 'postgresql+psycopg2://postgres:Exeteruni1#@eda.cvmmkqociyon.eu-north-1.rds.amazonaws.com:5432/telegram_bot'
+DATABASE_URL = 'postgresql+psycopg2://postgres:Exeteruni1#@database-1.c18ec4wiiab3.ap-south-1.rds.amazonaws.com:5432/mydb'
