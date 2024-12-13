42048367272cba9b30208e0c26f2e59ce4d506c7
Eldor <126213105+Eldor007@users.noreply.github.com>
2024-12-14 04:20:23 +0500

    Update user_bot.py

--- bots/user_bot.py
+++ bots/user_bot.py
@@ -12,7 +12,7 @@ import os
 from geopy.distance import geodesic
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from eda.db.models import Cafe  # Ваша модель для кафе
+from project_root.eda.db.models import Cafe  # Ваша модель для кафе
 import pandas as pd
 
 # Добавляем путь к корневой директории проекта
